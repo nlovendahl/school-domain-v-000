5eca4b3d47539258abe9240a59f40ed38deb198e
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-16 18:13:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   end
 
   def add_student(name, grade)
-    if ROSTER[grade]
+    if ROSTER[grade] && ROSTER[grade].include?(name) == false
       ROSTER[grade] << name
     else
       ROSTER[grade] = []
